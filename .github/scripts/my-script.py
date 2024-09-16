c330fae4114171b6141ed624ecca68545e1fea07
Raisul Islam Shehab <61587654+Raisul-Islam-Shehab@users.noreply.github.com>
2024-09-16 16:31:41 +0600

    Updated my-script.py

--- .github/scripts/my-script.py
+++ .github/scripts/my-script.py
@@ -1,1 +1,19 @@
-print("I'm runnnig a python script.")
+from github import Github
+
+# Authentication is defined via github.Auth
+from github import Auth
+
+# using an access token
+auth = Auth.Token("GITHUB_TOKEN")
+
+# Public Web Github
+g = Github(auth=auth)
+    
+repo = g.get_repo("Raisul-Islam-Shehab/LearnPython")
+print(repo.name)
+
+branches = list(repo.get_branches())
+print(branches)
+
+main_branch = repo.get_branch(branch="main")
+print(main_branch.commit)
