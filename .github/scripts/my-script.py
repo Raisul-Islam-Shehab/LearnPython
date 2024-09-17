4bcbbf27e5e3611fa415439f87933e8a758ec3eb
Raisul Islam Shehab <raisul.shehab@gmail.com>
2024-09-17 17:17:23 +0600

    New Commit

--- .github/scripts/my-script.py
+++ .github/scripts/my-script.py
@@ -8,20 +8,37 @@ token = os.getenv("GITHUB_TOKEN")
 
 if not token:
     raise ValueError("GitHub token not found in environment variables!")
-    
+
 # using an access token
-auth = Auth.Token(token=token)
+auth = Auth.Token(token)
 
 # Public Web Github
 g = Github(auth=auth)
-    
-repo = g.get_repo("Raisul-Islam-Shehab/LearnPython")
-print(repo.name)
 
-branches = list(repo.get_branches())
-print(branches)
 
-main_branch = repo.get_branch(branch="main")
-print(main_branch.commit)
+def getLatestCommit(repoName):
+    repo = g.get_repo(repoName)
+    latestCommit = repo.get_commits()[0]
+    print(f"Latest commit SHA: {latestCommit.sha}")
+    print(f"Latest commit message: {latestCommit.commit.message}")
+
+
+def listOfOpenIssues(repoName):
+    repo = g.get_repo(repoName)
+    open_issues = repo.get_issues(state="open")
+    for issue in open_issues:
+        print(issue)
+
+
+def listPullRequests(repoName):
+    repo = g.get_repo(repoName)
+    pulls = repo.get_pulls(state="open", sort="created", base="main")
+    for pr in pulls:
+        print(pr)
+
+
+getLatestCommit("Raisul-Islam-Shehab/LearnPython")
+listOfOpenIssues("Raisul-Islam-Shehab/LearnPython")
+listPullRequests("Raisul-Islam-Shehab/LearnPython")
 
 g.close()
