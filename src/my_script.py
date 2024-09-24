86add5e0cbea2be4eda60552d356a140afbdd1ff
Raisul Islam Shehab <raisul.shehab@gmail.com>
2024-09-24 20:19:57 +0600

    Updated all

--- src/my_script.py
+++ src/my_script.py
@@ -1,47 +1,116 @@
 import requests
+import re
+import pprint
 import os
-import json
 
 token = os.getenv("GITHUB_TOKEN")
 
-url = "https://api.github.com/repos/Raisul-Islam-Shehab/LearnPython"
+repo_url = "https://api.github.com/repos/Raisul-Islam-Shehab/LearnPython"
 
-headers = {
+github_headers = {
     "Accept": "application/vnd.github+json",
     "X-GitHub-Api-Version": "2022-11-28",
     "Authorization": f"Bearer {token}",
 }
 
 
+# Function to fetch paginated data
+def get_paginated_data(url, headers=None, params=None):
+    next_pattern = re.compile(r'(?<=<)([\S]*)(?=>; rel="next")', re.IGNORECASE)
+    pages_remaining = True
+    data = []
+
+    # Continue fetching pages as long as there is a next page
+    while pages_remaining:
+        response = requests.get(f"{url}", headers=headers, params=params)
+
+        # Parse the response JSON data
+        parsed_data = parse_data(response.json())
+        data.extend(parsed_data)
+
+        # Check the 'link' header for pagination
+        link_header = response.headers.get("link")
+        pages_remaining = link_header and 'rel="next"' in link_header
+
+        # Update the URL for the next page if it exists
+        if pages_remaining:
+            url = re.search(next_pattern, link_header).group(0)
+
+    return data
 
 
-# def getLatestCommit():
-#     response = requests.get(f"{url}/commits", headers=headers)
-#     # commits = response.json()
-#     # latestCommit = commits[0]
-#     # # print(latestCommit['commit']['author']['name'])
-    
-#     # commits_pretty = json.dumps(latestCommit, indent=4)
-#     # print(commits_pretty)
-    
-#     # return latestCommit
-#     # print(type(response.status_code))
-#     return response
+# Function to parse data from the response
+def parse_data(data):
+    # If data is already a list, return it
+    if isinstance(data, list):
+        return data
+
+    # Return empty list if there's no data
+    if not data:
+        return []
+
+    # Remove unwanted keys from the response
+    data.pop("incomplete_results", None)
+    data.pop("repository_selection", None)
+    data.pop("total_count", None)
+
+    # Extract the first key which contains the array of items
+    namespace_key = list(data.keys())[0]
+    # print(namespace_key)
+    return data[namespace_key]
 
 
-def getOpenIssues():
-    page = 1
-    response = requests.get(f"{url}/issues", headers=headers)
-    issues = response.json()
-    # commits_pretty = json.dumps(issues, indent=4) // returns sting that represents JSON
-    # print(commits_pretty)
-    # print(issues[0]['title'])
-    # print(issues[0]['state'])
+def get_latest_commit():
+    response = requests.get(
+        f"{repo_url}/commits", headers=github_headers, params={"per_page": 1}
+    )
+    latestCommit = response.json()
+    return latestCommit
+
+
+def get_open_issues():
+    # Fetch data using pagination
+    issues = get_paginated_data(f"{repo_url}/issues", github_headers, {"per_page": 1})
+    # pprint.pprint(issues)
     return issues
+
+
+def get_pull_requests():
+    pull_requests = get_paginated_data(
+        f"{repo_url}/pulls", github_headers, {"per_page": 1}
+    )
+    # pprint.pprint(pull_requests)
+    return pull_requests
+
+
+def print_latest_commit():
+    latestCommit = get_latest_commit()
+    pprint.pprint(latestCommit)
     
 
-    
-getLatestCommit()
-# getOpenIssues()
+def print_open_issues_details():
+    open_issues = get_open_issues()
+    i = 1
+    for issue in open_issues:
+        print(f"Issue {i}: {issue['title']:}")
+        print(f"\t Description: {issue['body']}")
+        print(f"\t Status: {issue['state']}")
+        i += 1
 
 
+
+def print_pull_requests_details():
+    pull_requests = get_pull_requests()
+    i = 1
+    for request in pull_requests:
+        print(f"Pull_Request {i}: {request['title']:}")
+        print(f"\t Description: {request['body']}")
+        print(f"\t Status: {request['state']}")
+        i += 1
+        
+
+# print_open_issues_details()
+# print_latest_commit()
+# print_pull_requests_details()
+get_open_issues()
+        
--- src/test_script.py
+++ src/test_script.py
@@ -1,28 +1,15 @@
-from my_script import getLatestCommit
-# import pytest
-import unittest
-
-class TestMyScript(unittest.TestCase):
-    def test_status(self):
-        response = getLatestCommit()
-        self.assertEqual(response.status_code, 200)
-    
-    def test_committer(self):
-        response = getLatestCommit()
-        commits = response.json()
-        latestCommit = commits[0]
-        # print(latestCommit)
-        self.assertEqual(latestCommit['commit']['committer']['name'], "Raisul Islam Shehab")
-    
-    # def test_addition(self):
-    #     self.assertEqual(my_script.addition(2,3), 5)
+import my_script
+import pytest
 
 
-if __name__ == "__main__":
-    unittest.main(verbosity=2)
+def test_get_latest_commit():
+    response = my_script.get_latest_commit()
+    assert response[0]["commit"]["committer"]["name"] == "Raisul Islam Shehab"
 
-# def test_addition():
-#     assert my_script.addition(2,3) == 5
 
-# python -m coverage run -m unittest
-# python -m coverage report
+def test_get_open_issues():
+    response = my_script.get_open_issues()
+    assert (
+        response[0]["repository_url"]
+        == "https://api.github.com/repos/Raisul-Islam-Shehab/LearnPython"
+    )
