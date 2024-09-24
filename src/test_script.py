0d061d9c4761dd9b2112410f6a3fcbed80dd2eed
Raisul Islam Shehab <raisul.shehab@gmail.com>
2024-09-24 20:46:24 +0600

    Test file updated

--- src/test_script.py
+++ src/test_script.py
@@ -1,14 +1,23 @@
 import my_script
+import pprint
 
 
 def test_get_latest_commit():
     response = my_script.get_latest_commit()
-    assert response[0]["commit"]["committer"]["name"] == "Raisul Islam Shehab"
+    latestCommit = response[0]
+    # pprint.pprint(response)
+    # print(latestCommit['commit'])
+    assert latestCommit["commit"]["committer"]["name"] == "Raisul Islam Shehab"
 
 
 def test_get_open_issues():
     response = my_script.get_open_issues()
+    latestIssue = response[0]
+    # pprint.pprint(latestIssue['repository_url'])
     assert (
-        response[0]["repository_url"]
+        latestIssue["repository_url"]
         == "https://api.github.com/repos/Raisul-Islam-Shehab/LearnPython"
     )
+
+# test_get_latest_commit()
+# test_get_open_issues()
