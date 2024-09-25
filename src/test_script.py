cd1af43e1b6206435c54a7a70a259204f0517a68
Raisul Islam Shehab <raisul.shehab@gmail.com>
2024-09-25 18:29:45 +0600

    New Update

--- src/test_script.py
+++ src/test_script.py
@@ -1,31 +1,52 @@
 import my_script
+import unittest
 
-# import pprint
-
-
-def test_get_latest_commit():
-    response = my_script.get_latest_commit()
-    # print(type(response))
-    # pprint.pprint(response)
-    latestCommit = response.json()[0]
-    print(type(latestCommit))
-    # pprint.pprint(response)
-    print(latestCommit["commit"]["committer"]["name"])
-    # assert latestCommit["commit"]["committer"]== "Raisul Islam Shehab"
+# # import pprint
 
 
-def test_get_open_issues():
-    response = my_script.get_open_issues()
-    # print(type(response))
-    latestIssue = response[0]
-    # pprint.pprint(latestIssue['repository_url'])
-    print(type(latestIssue))
-    print(latestIssue["repository_url"])
-    # assert (
-    #     latestIssue["repository_url"]
-    #     == "https://api.github.com/repos/Raisul-Islam-Shehab/LearnPython"
-    # )
+# def test_get_latest_commit():
+#     response = my_script.get_latest_commit()
+#     # print(type(response))
+#     # pprint.pprint(response)
+#     latestCommit = response.json()[0]
+#     print(type(latestCommit))
+#     # pprint.pprint(response)
+#     print(latestCommit["commit"]["committer"]["name"])
+#     # assert latestCommit["commit"]["committer"]== "Raisul Islam Shehab"
 
 
-test_get_latest_commit()
-test_get_open_issues()
+# def test_get_open_issues():
+#     response = my_script.get_open_issues()
+#     # print(type(response))
+#     latestIssue = response[0]
+#     # pprint.pprint(latestIssue['repository_url'])
+#     print(type(latestIssue))
+#     print(latestIssue["repository_url"])
+#     # assert (
+#     #     latestIssue["repository_url"]
+#     #     == "https://api.github.com/repos/Raisul-Islam-Shehab/LearnPython"
+#     # )
+
+
+# test_get_latest_commit()
+# test_get_open_issues()
+
+
+class TestMyScript(unittest.TestCase):
+    def test_get_latest_commit(self):
+        response = my_script.get_latest_commit()
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_open_issues(self):
+        issues = my_script.get_open_issues()
+
+        latestIssue = issues[0]
+        # print(latestCommit)
+        self.assertEqual(
+            latestIssue["repository_url"],
+            "https://api.github.com/repos/Raisul-Islam-Shehab/LearnPython",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
